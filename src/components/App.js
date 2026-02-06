0b2607a68924958b9a2edef302543104eb72a7bd
vchhavi68 <vchhavi68@gmail.com>
2026-02-06 17:46:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./home";
+import Header from "./header";
+import Admin from "./admin";
+import Product from "./product";
+import AdminProdcut from "./adminProduct";
+
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/products/:id" element={<Product />} />
+        <Route path="/admin/products/:id" element={<AdminProdcut />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
