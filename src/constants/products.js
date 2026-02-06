a85d9420584880dcd66a2f4b72059d07461990c1
vchhavi68 <vchhavi68@gmail.com>
2026-02-06 22:18:16 +0530

    Create products.js

---
+++ src/constants/products.js
@@ -0,0 +1,9 @@
+const products = [
+  { id: 1, name: "Mobile 1", description: "Description 1", price: "$200" },
+  { id: 2, name: "Mobile 2", description: "Description 2", price: "$300" },
+  { id: 3, name: "Mobile 3", description: "Description 3", price: "$400" },
+  { id: 4, name: "Mobile 4", description: "Description 3", price: "$400" },
+  { id: 5, name: "Mobile 5", description: "Description 3", price: "$400" },
+];
+
+export default products;
