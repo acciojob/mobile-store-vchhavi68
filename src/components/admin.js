b90af84e8c8c6816a9a5ae6f84a24812056f8de5
vchhavi68 <vchhavi68@gmail.com>
2026-02-06 22:03:27 +0530

    Update admin.js

--- src/components/admin.js
+++ src/components/admin.js
@@ -1,1 +1,20 @@
+import React from "react";
+import products from "../constants/products";
+import { Link } from "react-router-dom";
 
+function Admin() {
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+      <ul>
+        <p key={products[0].id}>
+          <Link to={`/admin/products/${products[0].id}`}>
+            {products[0].name} - ${products[0].price}
+          </Link>
+        </p>
+      </ul>
+    </div>
+  );
+}
+
+export default Admin;
