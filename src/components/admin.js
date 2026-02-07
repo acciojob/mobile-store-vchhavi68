6ce9fe91d0eb9b85af191b46d06cf5a53423381f
vchhavi68 <vchhavi68@gmail.com>
2026-02-07 12:56:07 +0530

    Update admin.js

--- src/components/admin.js
+++ src/components/admin.js
@@ -6,14 +6,13 @@ function Admin() {
   return (
     <div>
       <h1>Admin Panel</h1>
-
-      {products.map((item) => (
-        <div key={item.id}>
-          <Link to={`/admin/products/${item.id}`}>
-            {item.name} - {item.price}
+      <ul>
+        <p key={products[0].id}>
+          <Link to={`/admin/products/${products[0].id}`}>
+            {products[0].name} - ${products[0].price}
           </Link>
-        </div>
-      ))}
+        </p>
+      </ul>
     </div>
   );
 }
